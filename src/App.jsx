fa4b1e9113d3cbc391968b4aed8bfdd656d6149d
BRUNO_PESSOA <202503440964>
2025-06-13 09:19:10 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,19 @@ import Footer from './components/Footer';
 import Home from './pages/Home';
 import Inscricao from './pages/Inscricao';
 import Sucesso from './pages/Sucesso';
-import Sobre from './pages/Sobre';
-import Contato from './pages/Contato';
 import './App.css'; 
 
 function App() {
   return (
     <Router>
       <Header />
-      <nav style={{ marginBottom: 20 }}>
-        <Link to="/">Home</Link> |{' '}
-        <Link to="/sobre">Sobre</Link> |{' '}
-        <Link to="/contato">Contato</Link>
-      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/inscricao" element={<Inscricao />} />
         <Route path="/sucesso" element={<Sucesso />} />
-        <Route path="/sobre" element={<Sobre />} />
-        <Route path="/contato" element={<Contato />} />
       </Routes>
       <Footer />
     </Router>
   );
 }
-
 export default App;
